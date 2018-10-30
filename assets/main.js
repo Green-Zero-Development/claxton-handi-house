59f785e801225ec8dea7ab34f713fcb9b286cbdf
Animated-Eagle <sheddinghorribly@gmail.com>
2018-10-30 16:56:43 -0400

    first commit

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+---
+# Only the main Sass file needs front matter (the dashes are enough)
+---
+
+{% include /js/jquery.min.js %}
+
+{% include /js/tether.min.js %}
+
+{% include /js/toolkit.min.js %}
+
+{% include /js/application.js %}
+
+
+
+
+
